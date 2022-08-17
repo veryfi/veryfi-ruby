e8180a4dba13ee74189a04abd497f397e52c661a
Sebastian Carmona <sebastiancarmona@hotmail.ca>
2022-08-17 12:41:47 -0500

    Use backend-message instead of custom class.

--- lib/veryfi/error.rb
+++ lib/veryfi/error.rb
@@ -3,43 +3,14 @@
 module Veryfi
   class Error
     def self.from_response(response)
-      case response.status
-      when 400 then BadRequest
-      when 401 then UnauthorizedAccessToken
-      when 405 then UnexpectedHTTPMethod
-      when 409 then AccessLimitReached
-      else InternalError
+      raise VeryfiError, response['error']
+    end
+
+    class VeryfiError < StandardError
+      def initialize(msg = "Veryfi Error")
+        super(msg)
       end
     end
 
-    class BadRequest < StandardError
-      def to_s
-        "Bad Request"
-      end
-    end
-
-    class UnauthorizedAccessToken < StandardError
-      def to_s
-        "Unauthorized Access Token"
-      end
-    end
-
-    class UnexpectedHTTPMethod < StandardError
-      def to_s
-        "Unexpected HTTP Method"
-      end
-    end
-
-    class AccessLimitReached < StandardError
-      def to_s
-        "Access Limit Reached"
-      end
-    end
-
-    class InternalError < StandardError
-      def to_s
-        "Internal Server Error"
-      end
-    end
   end
 end
