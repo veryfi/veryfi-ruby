b3b4a2fb983ffe8d7a0a86584fe9052fd1143e55
Sebastian <sebastiancarmona@hotmail.ca>
2024-06-11 09:54:08 -0500

    Add details

--- lib/veryfi/error.rb
+++ lib/veryfi/error.rb
@@ -11,53 +11,71 @@ module Veryfi
     end
 
     def self.get_error(status, response)
+      details = []
+      details = response["details"] if response.key?("details")
       case status
-      when 400 then BadRequest
-      when 401 then UnauthorizedAccessToken
-      when 404 then ResourceNotFound
-      when 405 then UnexpectedHTTPMethod
-      when 409 then AccessLimitReached
-      when 500 then InternalError
-      else VeryfiError.new(format("%<code>d, %<message>s", code: status, message: response["error"]))
-      end
-    end
-    class BadRequest < VeryfiError
-      def to_s
-        "400, Bad Request"
-      end
-    end
-
-    class UnauthorizedAccessToken < VeryfiError
-      def to_s
-        "401, Unauthorized Access Token"
-      end
-    end
-
-    class ResourceNotFound < VeryfiError
-      def to_s
-        "404, Resource not found"
-      end
-    end
-
-    class UnexpectedHTTPMethod < VeryfiError
-      def to_s
-        "405, Unexpected HTTP Method"
-      end
-    end
-
-    class AccessLimitReached < VeryfiError
-      def to_s
-        "409, Access Limit Reached"
-      end
-    end
-
-    class InternalError < VeryfiError
-      def to_s
-        "500, Internal Server Error"
+      when 400 then BadRequest.new(details)
+      when 401 then UnauthorizedAccessToken.new(details)
+      when 404 then ResourceNotFound.new(details)
+      when 405 then UnexpectedHTTPMethod.new(details)
+      when 409 then AccessLimitReached.new(details)
+      when 500 then InternalError.new(details)
+      else VeryfiError.new(format("%<code>d, %<message>s", code: status, message: response["error"]), details)
       end
     end
 
     class VeryfiError < StandardError
+      attr_reader :details, :message
+
+      def initialize(message = "An error occurred", details = [])
+        @message = message
+        @details = if details.empty?
+          ""
+        else
+          "\n#{details.map { |val| val['msg']}.join("\n")}"
+        end
+        super(message)
+      end
+
+      def to_s
+        "#{@message}#{@details}"
+      end
+    end
+
+    class BadRequest < VeryfiError
+      def initialize(details = [])
+        super("400, Bad Request", details)
+      end
+    end
+
+    class UnauthorizedAccessToken < VeryfiError
+      def initialize(details = [])
+        super("401, Unauthorized Access Token", details)
+      end
+    end
+
+    class ResourceNotFound < VeryfiError
+      def initialize(details = [])
+        super("404, Resource not found", details)
+      end
+    end
+
+    class UnexpectedHTTPMethod < VeryfiError
+      def initialize(details = [])
+        super("405, Unexpected HTTP Method", details)
+      end
+    end
+
+    class AccessLimitReached < VeryfiError
+      def initialize(details = [])
+        super("409, Access Limit Reached", details)
+      end
+    end
+
+    class InternalError < VeryfiError
+      def initialize(details = [])
+        super("500, Internal Server Error", details)
+      end
     end
   end
 end
